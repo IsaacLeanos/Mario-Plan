7884b7eb31f9465d57a78d1957a96267d42a2ef3
unknown <isaac.leanos11@gmail.com>
2018-09-27 03:40:12 -0700

    redux

--- mario-plan/src/component/dashboard/Dashboard.js
+++ mario-plan/src/component/dashboard/Dashboard.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 import Notification from './Notification'
 import ProjectList from '../project/ProjectList'
+import{connect}from'react-redux'
 
 class Dashboard extends Component {
   render() {
+    // console.log(this.props)
+    const {projects}=this.props
     return (
       <div                       className='dashboard container'>
         <div                     className='row'>
         <div                     style={{"border":"1px solid red;"}} className='col s12 m6'>
-        <ProjectList/>
+        <ProjectList             projects={projects}/>
         </div>
 
         <div                     style={{"border":"1px solid red;"}} className='col s12 m5 off-set-m1'>
@@ -20,4 +23,8 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard
+const mapStateToProps=(state)=>({
+  projects:state.projectReducer.projects
+})
+
+export default connect(mapStateToProps)(Dashboard)
--- mario-plan/src/component/project/ProjectSummary.js
+++ mario-plan/src/component/project/ProjectSummary.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const ProjectSummary=()=>{
+const ProjectSummary=({project})=>{
     return (
       <div className='project-list section'>
 
       <div className='card  project-summary'>
       <div className='card-content grey-text text-darken-3'>
-      <span className='card-title'>Project Title</span>
-      <p>Posted by: Me</p>
+      <span className='card-title'>{project.title}</span>
+      <p>{project.content}</p>
       <p className='grey-text'>Sep 19 2:am</p>
       </div>
       </div>
